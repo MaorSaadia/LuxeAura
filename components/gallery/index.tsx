d75503c0bac0bd5a1786a31b32600f5957efef8f
Maor <maorsa9@gmail.com>
2024-09-08 20:05:53 +0300

    impleamnt the Individual product screen

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,41 +1,40 @@
 "use client";
 
 import Image from "next/image";
-import { Tab } from "@headlessui/react";
+import { TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 import { Image as ImageType } from "@/types";
 import GalleryTab from "./gallery-tab";
 
 interface GalleryProps {
   images: ImageType[];
-
 }
-const Gallery: React.FC<GalleryProps> = ({
-  images
-}) => {
-  return ( 
-    <Tab.Group as="div" className="flex flex-col-reverse">
+const Gallery: React.FC<GalleryProps> = ({ images }) => {
+  return (
+    <TabGroup as="div" className="flex flex-col-reverse">
       <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-        <Tab.List className="grid grid-cols-3 gap-6">
+        <TabList className="grid grid-cols-3 gap-6">
           {images.map((image) => (
             <GalleryTab key={image.id} image={image} />
           ))}
-        </Tab.List>
+        </TabList>
       </div>
-      <Tab.Panels className="aspect-square w-full">
+      <TabPanels className="aspect-square w-full">
         {images.map((image) => (
-          <Tab.Panel key={image.id}>
+          <TabPanel key={image.id}>
             <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
-              <Image fill src={image.url} alt="Image" className="object-cover object-center"/>
-
+              <Image
+                fill
+                src={image.url}
+                alt="Image"
+                className="object-cover object-center"
+              />
             </div>
-
-          </Tab.Panel>
+          </TabPanel>
         ))}
+      </TabPanels>
+    </TabGroup>
+  );
+};
 
-      </Tab.Panels>
-    </Tab.Group>
-   );
-}
- 
 export default Gallery;
