90b79a7b726bd62f4ce018fe9e66c25e611b6f9c
Maor <maorsa9@gmail.com>
2024-09-08 19:32:57 +0300

    working with getting the products and diplaying it in the store

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,23 +1,22 @@
-import getBillboard from "@/actions/get-billboards";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
-
-// import getProducts from "@/actions/get-products";
-// import ProductList from "@/components/product-list";
+import ProductList from "@/components/product-list";
+import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-  //   const products = await getProducts({ isFeatured: true });
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("f4aa02c1-73c5-4e17-901f-466d2e7bd8e1");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-        {/* <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
-        </div> */}
+        </div>
       </div>
     </Container>
   );
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
-        <p className="text-center text-xs text-black">
+        <p className="text-center sm:text-xs md:text-sm text-black">
           &copy; {new Date().getFullYear()} LuxeAura, Inc. All rights reserved.
         </p>
       </div>
