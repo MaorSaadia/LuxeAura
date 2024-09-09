bdf05d51f2d3e0aa773cb18359a1413d95d213a1
Maor <maorsa9@gmail.com>
2024-09-09 10:12:06 +0300

    added Product preview modal components

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Image from "next/image";
-// import { MouseEventHandler } from "react";
+import { MouseEventHandler } from "react";
 import { Expand, ShoppingCart } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
-// import usePreviewModal from "@/hooks/use-preview-modal";
+import usePreviewModal from "@/hooks/use-preview-modal";
 // import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
@@ -16,18 +16,18 @@ interface ProductCard {
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
-  // const previewModal = usePreviewModal();
+  const previewModal = usePreviewModal();
   // const cart = useCart();
   const router = useRouter();
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   };
 
-  // const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
-  //   event.stopPropagation();
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
 
-  //   previewModal.onOpen(data);
-  // };
+    previewModal.onOpen(data);
+  };
 
   // const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
   //   event.stopPropagation();
@@ -50,7 +50,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              // onClick={onPreview}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
